0de7cd374b0f340342640a69935e1774e30314b1
FLYNNBRIGHT <flynnbright@gmail.com>
2023-03-21 15:34:25 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- *  main - Entry point
+ * Main -Entry point
  *
- *  Return:  Always 0 (success)
+ * Return: Akways 0 (Success)
  */
 int main(void)
 {
-	 printf("with proper grammar, but the outcome is a piece of art,\n");
-	 return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
