1f4da0a4010f259156a269c3da38b7e1a2d9fcd6
FLYNNBRIGHT <flynnbright@gmail.com>
2023-03-23 10:39:29 -0700

    111

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,9 +10,10 @@
 int main(void)
 {
 	int n;
+	int x;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2:
+	n = rand() - RAND_MAX / 2;
 	x = n % 10;
 	printf("Last digit of %d is %d", n, x);
 	if (x > 5)
@@ -23,10 +24,10 @@ int main(void)
 	{
 		printf("and  is 0");
 	}
-	else (x < 6 && x != 0)
+	else (x < 6 && x != 0);
 	{
 		printf("and is less than 6 and not 0");
 	}
 	printf("\n");
-
 	return (0);
+}
