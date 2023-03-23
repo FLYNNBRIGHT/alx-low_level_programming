852a87847247f0236ad5b17fc77edb6ab0b69b64
FLYNNBRIGHT <flynnbright@gmail.com>
2023-03-23 12:51:04 -0700

    22

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - prints the letter in lower case and then in uppercase
+  *
+  * Return: Always 0 (Success).
+  */
+char main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
