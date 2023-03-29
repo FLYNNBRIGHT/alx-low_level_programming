3b89ac27f021f4ac936e4f86094431dd076b7356
FLYNNBRIGHT <flynnbright@gmail.com>
2023-03-29 06:46:57 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: char to check
+ * Description: This will return the length of the string
+ * Retutn: Always 0 (Success).
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s++;)
+		a++;
+	return (0);
+}
