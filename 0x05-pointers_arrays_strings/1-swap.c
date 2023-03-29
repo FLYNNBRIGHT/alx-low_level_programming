f42a6540411edb5cbd3de6309c42d663324e603b
FLYNNBRIGHT <flynnbright@gmail.com>
2023-03-29 06:37:55 -0700

    alll

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of int a and int b.
+ * @a: first  int.
+ * @b: secong int.
+ * Return: Always 0 (Success).
+ */
+void swap_int(int *a, int *b)
+{
+	int s;
+
+	s = *a;
+	*a = *b;
+	*b = s;
+}
