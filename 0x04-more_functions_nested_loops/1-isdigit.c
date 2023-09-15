777c50857a7dade01b80454883c50ef98128df4c
FLYNNBRIGHT <flynnbright@gmail.com>
2023-09-14 23:03:54 -0700

    Fix the description error with script 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,13 +4,15 @@
  * _isdigit - Checks for a digit (0 through 9).
  * @c: character to be checked.
  *
- * Returns: (1) for a digit and (0) for a non digit.
+ * Return: 1 for a digit and 0 for a non digit.
  */
+
 
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
+
 	else
 		return (0);
 }
